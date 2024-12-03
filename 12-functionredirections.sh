7264f4f0d45f0d22abf2204330c76e4f3fe7e6da
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-12-03 13:02:10 +0530

    functions

---
+++ 12-functionredirections.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(DATE +%F-%H-%M-%S)
+SCRIPTNAME=$($0 \ cut -d "." -f1)    # $0  is fto find the script name 
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log   #/ creating a log file 
+
+VALIDATE(){
+   if [$1 -ne 0 ]
+   then 
+   echo "$2....failure"
+   exit 1
+   else 
+   echo "$2 ....success"
+   fi 
+}
+
+if [USERID -ne 0]
+then 
+echo "please run this script with root access"
+exit1
+else
+echo "you are a super user"
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "installing mysql"
+
+dnf install git -y  &>>$LOGFILE
+VALIDATE $? "installing git"
