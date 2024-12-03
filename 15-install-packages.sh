de2f7ff549052bfd1a46dfbf680496f6c4625d92
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-12-03 14:31:52 +0530

    functions

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)    # $0  is fto find the script name 
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log   #/ creating a log file 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then 
+   echo -e "$2....$R  failure  $N"
+   exit 1
+   else 
+   echo -e "$2 ....$G  success $N"
+   fi 
+}
+
 
 if [ $USERID -ne 0 ]
 then 
@@ -14,5 +32,14 @@ fi
 
 for i in $@
 do 
-echo "install $i"
+echo "package to install $i"
+dnf  list installed $i &>>$LOGFILE   
+if [$? -eq 0]
+then 
+echo -e "$i already installed .... $Y SKIPPING $N"
+else
+dnf install $i -y &>>$LOGFILE
+VALIDATE $? "installation of $i"
+fi
+
 done
