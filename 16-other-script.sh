0bc64bacfb97d99a70381ba87d3c25b178bf2929
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-12-18 22:44:40 +0530

    functions

--- 16-other-script.sh
+++ 16-other-script.sh
@@ -8,11 +8,4 @@ echo "processid of other script:$$"
 #echo "variable value from other script : $COURSE" 
 
 
-#!/bin/bash
-
-COURSE="DevOps from Other Script"
-
-echo "Variable value from Other script: $COURSE"
-echo "Process ID of other script: $$"
-
 
