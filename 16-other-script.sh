977c06378587e32b3b799c8f159718f6abeedc86
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-12-18 19:29:53 +0530

    functions

---
+++ 16-other-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COURSE="devops fromm other script"
+
+echo "variable value from other script : $COURSE"
+echo "processid of other script:$$"
+
+#echo "variable value from other script : $COURSE" 
--- 17-current-script.sh
+++ 17-current-script.sh
@@ -5,6 +5,6 @@ COURSE="deVOPS FROM CURRENT SCRIPT"
 echo  "BEFORE CALLING OTHER SCRIPTCOURSE : $COURSE"
 echo  "process id of current script: $$"
 
-./16-otherscript.sh
+sourse ./16-otherscript.sh
 
 echo "after calling other script, course: $COURSE "
