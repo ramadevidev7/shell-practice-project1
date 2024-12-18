f6ad1fafd0f9c4c598f271eb7857d2115d2ea30a
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-12-18 19:30:40 +0530

    functions

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -5,6 +5,6 @@ COURSE="deVOPS FROM CURRENT SCRIPT"
 echo  "BEFORE CALLING OTHER SCRIPTCOURSE : $COURSE"
 echo  "process id of current script: $$"
 
-sourse ./16-otherscript.sh
+sourse ./16-other-script.sh
 
 echo "after calling other script, course: $COURSE "
